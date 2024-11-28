b81d4fa62dfa03e6872796a2bc7b99aa0b298206
panha <huypanha558@gmail.com>
2024-11-28 15:13:31 +0700

    Updated dependencies

--- lib/blue_print_pos.dart
+++ lib/blue_print_pos.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:blue_print_pos/models/models.dart';
@@ -10,14 +9,13 @@ import 'package:blue_thermal_printer/blue_thermal_printer.dart' as blue_thermal;
 import 'package:esc_pos_utils_plus/esc_pos_utils.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart' as flutter_blue;
-import 'package:flutter_blue_plus/gen/flutterblueplus.pb.dart' as proto;
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:image/image.dart' as img;
 import 'package:qr_flutter/qr_flutter.dart';
 
 class BluePrintPos {
   BluePrintPos._() {
     _bluetoothAndroid = blue_thermal.BlueThermalPrinter.instance;
-    _bluetoothIOS = flutter_blue.FlutterBluePlus.instance;
   }
 
   static BluePrintPos get instance => BluePrintPos._();
@@ -26,9 +24,6 @@ class BluePrintPos {
 
   /// This field is library to handle in Android Platform
   blue_thermal.BlueThermalPrinter? _bluetoothAndroid;
-
-  /// This field is library to handle in iOS Platform
-  flutter_blue.FlutterBluePlus? _bluetoothIOS;
 
   /// Bluetooth Device model for iOS
   flutter_blue.BluetoothDevice? _bluetoothDeviceIOS;
@@ -63,19 +58,9 @@ class BluePrintPos {
                 selectedDevice?.name ?? '', selectedDevice?.address ?? '');
         await _bluetoothAndroid?.connect(bluetoothDeviceAndroid);
       } else if (Platform.isIOS) {
-        _bluetoothDeviceIOS = flutter_blue.BluetoothDevice.fromProto(
-          proto.BluetoothDevice(
-            name: selectedDevice?.name ?? '',
-            remoteId: selectedDevice?.address ?? '',
-            type: proto.BluetoothDevice_Type.valueOf(selectedDevice?.type ?? 0),
-          ),
-        );
-        final List<flutter_blue.BluetoothDevice> connectedDevices =
-            await _bluetoothIOS?.connectedDevices ??
-                <flutter_blue.BluetoothDevice>[];
-        final int deviceConnectedIndex = connectedDevices
-            .indexWhere((flutter_blue.BluetoothDevice bluetoothDevice) {
-          return bluetoothDevice.id == _bluetoothDeviceIOS?.id;
+        _bluetoothDeviceIOS = flutter_blue.BluetoothDevice.fromId(selectedDevice?.address ?? '');
+        final int deviceConnectedIndex = FlutterBluePlus.connectedDevices.indexWhere((flutter_blue.BluetoothDevice bluetoothDevice) {
+          return bluetoothDevice.remoteId == _bluetoothDeviceIOS?.remoteId.str;
         });
         if (deviceConnectedIndex < 0) {
           await _bluetoothDeviceIOS?.connect();
@@ -254,8 +239,9 @@ class BluePrintPos {
         data: text,
         version: QrVersions.auto,
         gapless: false,
-        color: const Color(0xFF000000),
-        emptyColor: const Color(0xFFFFFFFF),
+        dataModuleStyle: const QrDataModuleStyle(
+          color: Color(0xFF000000),
+        ),
       ).toImage(size);
       final ByteData? byteData =
           await image.toByteData(format: ImageByteFormat.png);
